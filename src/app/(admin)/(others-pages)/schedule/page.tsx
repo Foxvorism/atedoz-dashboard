20a61de563ff626958cb81db9f25efc3dc7cf40c
Foxvorism <imjohani2005@gmail.com>
2025-04-18 08:46:49 +0700

    refactor: Change page title

--- src/app/(admin)/(others-pages)/schedule/page.tsx
+++ src/app/(admin)/(others-pages)/schedule/page.tsx
@@ -1,19 +1,20 @@
-import Calendar from "@/components/schedule/Schedule";
+import Schedule from "@/components/schedule/Schedule";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import { Metadata } from "next";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "Next.js Calender | TailAdmin - Next.js Dashboard Template",
+  title: "Schedule | Atedoz Space Admin Dashboard",
   description:
     "This is Next.js Calender page for TailAdmin  Tailwind CSS Admin Dashboard Template",
   // other metadata
 };
+
 export default function page() {
   return (
     <div>
-      <PageBreadcrumb pageTitle="Calendar" />
-      <Calendar />
+      <PageBreadcrumb pageTitle="Schedule" />
+      <Schedule />
     </div>
   );
 }
