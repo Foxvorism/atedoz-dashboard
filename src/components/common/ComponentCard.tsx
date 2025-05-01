655810f35bb351787cddc5c883e9d64a80788365
Foxvorism <imjohani2005@gmail.com>
2025-05-01 13:29:02 +0700

    feat: Chreate a go back button

--- src/components/common/ComponentCard.tsx
+++ src/components/common/ComponentCard.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Link from "next/link";
+
+import { GroupIcon } from "../../icons/index";
 
 interface ComponentCardProps {
   title: string;
   children: React.ReactNode;
   className?: string; // Additional custom classes for styling
   desc?: string; // Description text
+  href: string;
 }
 
 const ComponentCard: React.FC<ComponentCardProps> = ({
@@ -12,21 +16,33 @@ const ComponentCard: React.FC<ComponentCardProps> = ({
   children,
   className = "",
   desc = "",
+  href=""
 }) => {
   return (
     <div
       className={`rounded-2xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03] ${className}`}
     >
       {/* Card Header */}
-      <div className="px-6 py-5">
-        <h3 className="text-base font-medium text-gray-800 dark:text-white/90">
-          {title}
-        </h3>
-        {desc && (
-          <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
-            {desc}
-          </p>
-        )}
+      <div className="px-6 py-5 flex justify-between">
+        <div>
+          <h3 className="text-base font-medium text-gray-800 dark:text-white/90">
+            {title}
+          </h3>
+          {desc && (
+            <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
+              {desc}
+            </p>
+          )}
+        </div>
+        <div>
+          <Link href={href}>
+            <button 
+              className="flex justify-center items-center text-sm w-7 h-7 rounded-md font-bold border text-center p-2 bg-[var(--color-brand-600)] text-white hover:bg-[var(--color-brand-500)]"
+            >
+              X
+            </button>
+          </Link>
+        </div>
       </div>
 
       {/* Card Body */}
