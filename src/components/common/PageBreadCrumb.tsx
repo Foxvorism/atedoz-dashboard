028a4080f6424fa3abc5f919b7a1e4afad35a26c
Foxvorism <imjohani2005@gmail.com>
2025-05-01 13:29:56 +0700

    feat: Add pageTitle2

--- src/components/common/PageBreadCrumb.tsx
+++ src/components/common/PageBreadCrumb.tsx
@@ -13,7 +13,7 @@ const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle1, pageTitle2 }) =
         className="text-xl font-semibold text-gray-800 dark:text-white/90"
         x-text="pageName"
       >
-        {!pageTitle2 ? pageTitle1 : pageTitle2}
+        {!pageTitle2 ? pageTitle1 : pageTitle2 + " " + pageTitle1.slice(0, -1)}
       </h2>
       <nav>
         <ol className="flex items-center gap-1.5">
@@ -41,29 +41,37 @@ const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle1, pageTitle2 }) =
               </svg>
             </Link>
           </li>
-          <li className={`text-sm font-semibold ${pageTitle2 ? "text-gray-500" : "text-gray-800"} dark:text-white/90`}>
-            {pageTitle1}
-            {pageTitle2 && (
-              <svg
-                className="stroke-current"
-                width="17"
-                height="16"
-                viewBox="0 0 17 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M6.0765 12.667L10.2432 8.50033L6.0765 4.33366"
-                  stroke=""
-                  strokeWidth="1.2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            )}
+          <li>
+            <Link 
+              href={"/" + pageTitle1.toLocaleLowerCase()} 
+              className={`inline-flex items-center gap-1.5 text-sm ${pageTitle2 ? "text-gray-500" : "text-gray-800 font-semibold"} dark:text-white/90`}>
+              {pageTitle1}
+              {pageTitle2 && (
+                <svg
+                  className="stroke-current"
+                  width="17"
+                  height="16"
+                  viewBox="0 0 17 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M6.0765 12.667L10.2432 8.50033L6.0765 4.33366"
+                    stroke=""
+                    strokeWidth="1.2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              )}
+            </Link>
           </li>
-          <li className="text-sm text-gray-800 dark:text-white/90">
-            {pageTitle2}
+          <li>
+            <Link 
+              href={"/" + pageTitle1.toLocaleLowerCase() + "/" + pageTitle2?.toLocaleLowerCase()} 
+              className="text-sm text-gray-800 font-semibold dark:text-white/90">
+              {pageTitle2}
+            </Link>
           </li>
         </ol>
       </nav>
