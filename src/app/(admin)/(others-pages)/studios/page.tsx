d6fffbd927b5d316d94c887fe49a3ed444c986e7
Foxvorism <imjohani2005@gmail.com>
2025-05-02 13:13:25 +0700

    feat: Create a table for studio data

---
+++ src/app/(admin)/(others-pages)/studios/page.tsx
@@ -0,0 +1,20 @@
+import Studios from "@/components/studios/Studios";
+import PageBreadcrumb from "@/components/common/PageBreadCrumb";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+    title: "Studios | Atedoz Space Admin Dashboard",
+    description:
+      "This is Next.js Calender page for TailAdmin  Tailwind CSS Admin Dashboard Template",
+    // other metadata
+  };
+  
+  export default function page() {
+    return (
+      <div>
+        <PageBreadcrumb pageTitle2={null} pageTitle1="Studios" />
+        <Studios />
+      </div>
+    );
+  }
---
+++ src/components/studios/Studios.tsx
@@ -0,0 +1,137 @@
+import React from "react";
+import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+
+import {
+  ListIcon, PencilIcon, TrashBinIcon
+} from "../../icons/index";
+
+interface Studios {
+  id: number;
+  studio: string;
+  latitude: string;
+  longitude: string;
+  desc: string;
+}
+
+// Define the table data using the interface
+const tableData: Studios[] = [
+  {
+    id: 1,
+    studio: "Atedoz Space Bogor Baru",
+    latitude: "-6.591612319327254",
+    longitude: "106.81014535373673",
+    desc: "Cabang Bogor Baru",
+  },
+  {
+    id: 2,
+    studio: "Atedoz Space G.g Kelor",
+    latitude: "-6.580193548718884",
+    longitude: "106.77196462860871",
+    desc: "Cabang G.g Kelor",
+  },
+];
+
+export default function Studios() {
+  return (
+    <>
+      <Link href="/studios/input">
+        <button 
+          className="flex w-full justify-center items-center rounded-lg border h-auto text-center p-3 mb-4 bg-[var(--color-brand-600)] text-white hover:bg-[var(--color-brand-500)]"
+          >
+            <div className="mr-1">
+              <ListIcon />
+          </div>
+          Add a New Studio
+        </button>
+      </Link>
+
+      <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]">
+        <div className="max-w-full overflow-x-auto">
+          <div className="min-w-[1102px]">
+            <Table>
+              {/* Table Header */}
+              <TableHeader className="border-b border-gray-100 dark:border-white/[0.05]">
+                <TableRow>
+                  <TableCell
+                    isHeader
+                    className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                  >
+                    No
+                  </TableCell>
+                  <TableCell
+                    isHeader
+                    className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                  >
+                    Studio
+                  </TableCell>
+                  <TableCell
+                    isHeader
+                    className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                  >
+                    Latitude
+                  </TableCell>
+                  <TableCell
+                    isHeader
+                    className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                  >
+                    Longitude
+                  </TableCell>
+                  <TableCell
+                    isHeader
+                    className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                  >
+                    Description
+                  </TableCell>
+                  <TableCell
+                    isHeader
+                    className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                  >
+                    Action
+                  </TableCell>
+                </TableRow>
+              </TableHeader>
+
+              {/* Table Body */}
+              <TableBody className="divide-y divide-gray-100 dark:divide-white/[0.05]">
+                {tableData.map((studios, index) => (
+                  <TableRow key={studios.id}>
+                    <TableCell className="px-4 py-3 text-gray-800 text-start text-theme-sm dark:text-gray-400">
+                        {index + 1}
+                    </TableCell>
+                    <TableCell className="px-4 py-3 text-gray-800 text-start text-theme-sm dark:text-gray-400">
+                        { studios.studio}
+                    </TableCell>
+                    <TableCell className="px-4 py-3 text-gray-800 text-start text-theme-sm dark:text-gray-400">
+                        {studios.latitude}
+                    </TableCell>
+                    <TableCell className="px-4 py-3 text-gray-800 text-start text-theme-sm dark:text-gray-400">
+                        {studios.longitude}
+                    </TableCell>
+                    <TableCell className="px-4 py-3 text-gray-800 text-start text-theme-sm dark:text-gray-400">
+                        {studios.desc}
+                    </TableCell>
+                    <TableCell className="px-4 py-3 text-gray-800 text-start text-theme-sm dark:text-gray-400 flex items-center">
+                      <span className="w-4 menu-item-icon-warning mr-3 cursor-pointer">
+                        <PencilIcon />
+                      </span>
+                      <span className="w-4 menu-item-icon-error cursor-pointer">
+                        <TrashBinIcon />
+                      </span>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
