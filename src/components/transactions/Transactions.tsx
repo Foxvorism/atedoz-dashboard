eae80cb85530dab933432bc5b82bcc3a269c6548
Foxvorism <imjohani2005@gmail.com>
2025-05-06 16:14:10 +0700

    fix: Fix api request function

--- src/components/transactions/Transactions.tsx
+++ src/components/transactions/Transactions.tsx
@@ -1,4 +1,6 @@
 'use client';
+import axios from "axios";
+import Swal from "sweetalert2";
 import React,  { useEffect, useState } from "react";
 import {
     Table,
@@ -14,28 +16,6 @@ import {
 
 import Badge from "../ui/badge/Badge";
 import Image from "next/image";
-
-// interface Transactions {
-//     id: number;
-//     user: {
-//         id: number,
-//         name: string,
-//     };
-//     package: {
-//         id: number,
-//         name: string,
-//     }
-//     schedule: {
-//         id: number,
-//         date: string,
-//         time: string,
-//         studio: {
-//             id: number,
-//             name: string,
-//         }
-//     }
-//     status: string;
-// }
 
 interface Transactions {
   id: number;
@@ -63,65 +43,101 @@ interface Transactions {
   admin_confirmed: number;
 }
 
-
-
-// Define the table data using the interface
-// const tableData: Transactions[] = [
-//     {
-//         id: 1,
-//         user: {
-//             id: 1,
-//             name: "Velis"
-//         },
-//         package: {
-//             id: 1,
-//             name: "Studio in Bogor Baru Branch"
-//         },
-//         schedule: {
-//             id: 1,
-//             date: "5 May 2025",
-//             time: "10.00 - 11.00",
-//             studio: {
-//                 id: 1,
-//                 name: "Atedoz Space Bogor Baru Branch",
-//             }
-//         },
-//         status: "Pending",
-//     },
-// ];
-
-
-
 export default function Transactions() {
-  const [transactions, setTransactions] = useState<Transactions[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [transactions, setTransactionsData] = useState<Transactions[]>([]);
+
+  const getTransactionData = async () => {
+    try {
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_HOST}/orders`, {
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('access_token')}`,
+        }
+      });
+
+      if (Array.isArray(response.data)) {
+        setTransactionsData(response.data);
+      } else {
+        console.warn("⚠️ Format response tidak sesuai harapan:", response.data);
+      }
+
+      console.log(response.data)
+
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response && error.response.status === 401) {
+        Swal.fire({
+          icon: 'error',
+          title: error.response.data.message,
+          showConfirmButton: false,
+          timer: 1500
+        });
+      } else {
+        console.error("Server error:", error);
+        Swal.fire({
+          icon: 'error',
+          title: 'error terjadi',
+          text: 'mohon coba lagi nanti.',
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+    }
+  };
+
+  const deleteTransaction = async (id: number) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: 'Loading...',
+          text: 'Mohon tunggu sebentar...',
+          allowOutsideClick: false,
+          didOpen: () => {
+            Swal.showLoading();
+          }
+        });
+
+        try {
+          // Pastikan id yang dikirimkan adalah valid
+          const res = await axios.delete(
+            `${process.env.NEXT_PUBLIC_BACKEND_HOST}/orders/${id}`,
+            {
+              headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`,
+              }
+            }
+          );
+          await getTransactionData();
+          Swal.fire(
+            'Deleted!',
+            'Your Package has been deleted.',
+            'success'
+          );
+        } catch (error) {
+          console.error('Error while deleting the price:', error);
+          Swal.fire({
+            icon: 'error',
+            title: 'Failed to Delete',
+            text: 'Terjadi kesalahan saat menghapus paket.',
+          });
+        }
+      }
+    });
+  };
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/orders")
-      .then((res) => res.json())
-      .then((data) => {
-        setTransactions(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      });
+    getTransactionData();
   }, []);
   
   return (
     <>
-      <button 
-        className="flex w-full justify-center items-center rounded-lg border h-auto text-center p-3 mb-4 bg-[var(--color-brand-600)] text-white hover:bg-[var(--color-brand-500)]"
-        // onClick={handleAddPhotoClick}  // Open the second modal
-      >
-          <div className="mr-1">
-              <TaskIcon />
-          </div>
-          Add a New Transaction
-      </button>
-    
-
       <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]">
         <div className="max-w-full overflow-x-auto">
           <div className="min-w-[1102px]">
@@ -176,38 +192,48 @@ export default function Transactions() {
 
               {/* Table Body */}
               <TableBody className="divide-y divide-gray-100 dark:divide-white/[0.05]">
-                {transactions.map((transaction, index) => (
+              {transactions.length === 0 ? (
+                <TableRow>
+                  <TableCell
+                    colSpan={7}
+                    className="px-4 py-3 text-center text-theme-sm dark:text-gray-400"
+                  >
+                    <span className="text-gray-500">No data available</span>
+                  </TableCell>
+                </TableRow>
+              ) : (
+                transactions.map((transaction, index) => (
                   <TableRow key={index + 1}>
                     <TableCell className="px-4 py-3 text-gray-800 text-start text-theme-sm dark:text-gray-400">
-                        {index + 1}
+                      {index + 1}
                     </TableCell>
                     <TableCell className="px-4 py-3 text-gray-800 text-start text-theme-sm dark:text-gray-400">
-                        { transaction.user.name}
+                      {transaction.user.name}
                     </TableCell>
                     <TableCell className="px-4 py-3 text-gray-800 text-start text-theme-sm dark:text-gray-400">
-                        {transaction.package.nama_paket}
+                      {transaction.package.nama_paket}
                     </TableCell>
                     <TableCell className="px-4 py-3 text-gray-800 text-start text-theme-sm dark:text-gray-400">
-                        {transaction.order_date} | {transaction.start_time} - {transaction.end_time}
+                      {transaction.order_date} | {transaction.start_time} - {transaction.end_time}
                     </TableCell>
                     <TableCell className="px-4 py-3 text-gray-800 text-start text-theme-sm dark:text-gray-400">
-                        {transaction.photo_studio.nama_studio}
+                      {transaction.photo_studio.nama_studio}
                     </TableCell>
                     <TableCell className="px-4 py-3 text-gray-800 text-start text-theme-sm dark:text-gray-400">
-                    <Badge
-                      size="sm"
-                      color={
-                        transaction.status === "Paid"
-                          ? "success"
-                          : transaction.status === "Pending"
-                          ? "warning"
-                          : transaction.status === "Completed"
-                          ? "primary"
-                          : "error"
-                      }
-                    >
-                      {transaction.status}
-                    </Badge>
+                      <Badge
+                        size="sm"
+                        color={
+                          transaction.status === "Paid"
+                            ? "success"
+                            : transaction.status === "Pending"
+                            ? "warning"
+                            : transaction.status === "Completed"
+                            ? "primary"
+                            : "error"
+                        }
+                      >
+                        {transaction.status}
+                      </Badge>
                     </TableCell>
                     <TableCell className="px-4 py-3 text-gray-800 text-start text-theme-sm dark:text-gray-400 flex items-center">
                       <span className="w-4 menu-item-icon-warning mr-3 cursor-pointer">
@@ -218,7 +244,8 @@ export default function Transactions() {
                       </span>
                     </TableCell>
                   </TableRow>
-                ))}
+                ))
+              )}
               </TableBody>
             </Table>
           </div>
